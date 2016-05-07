2e17c309173f60f96bcbdaac47c9cfc321e11d37
santy50646 <santy1515@hotmail.com>
2016-05-07 12:48:59 -0500

    Correcciones Sonar TeamCity
    Se implementaron las correcciones que arrojo el Sonar después de
    vincularlo con el TeamCity.

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -24,11 +24,17 @@ class UserIdentity extends CUserIdentity
 			'SGSST'=>'ISIII',
 		);
 		if(!isset($users[$this->username]))
+		{
 			$this->errorCode=self::ERROR_USERNAME_INVALID;
+		}
 		elseif($users[$this->username]!==$this->password)
+		{
 			$this->errorCode=self::ERROR_PASSWORD_INVALID;
+		}
 		else
+		{
 			$this->errorCode=self::ERROR_NONE;
+		}
 		return !$this->errorCode;
 	}
 }
